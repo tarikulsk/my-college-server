f1e2e5d49a8d5fe39950a651bf17c35ebc163735
tarikulsk <tarikulsk4@gmai.com>
2023-07-24 22:29:00 +0530

    ok

--- index.js
+++ index.js
@@ -59,38 +59,6 @@ run().catch(console.dir);
 
 
 
-
-// async function run() {
-//     try {
-//         // Connect the client to the server	(optional starting in v4.7)
-//         await client.connect();
-
-//         const database = client.db("usersDB");
-//         const userCollection = database.collection("users");
-
-//         app.get('/users', async (req, res) => {
-//             const cursor = userCollection.find()
-//             const results = await cursor.toArray();
-//             res.send(results)
-//         })
-
-//         app.post('/users', async (req, res) => {
-//             const user = req.body;
-//             console.log('new user', user);
-//             const result = await userCollection.insertOne(user);
-//             res.send(result);
-//         })
-//         // Send a ping to confirm a successful connection
-//         await client.db("admin").command({ ping: 1 });
-//         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//     } finally {
-//         // Ensures that the client will close when you finish/error
-//         // await client.close();
-//     }
-// }
-// run().catch(console.dir);
-
-
 app.get('/', (req, res) => {
     res.send('Simple Crud is Running')
 
