ae9f1a290e42860f25e17762c9c628ac9f18e06e
tarikulsk <tarikulsk4@gmai.com>
2023-07-24 20:28:02 +0530

    applicaton

--- index.js
+++ index.js
@@ -2,28 +2,63 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const colleges = require('./data/colleges.json')
 
 app.use(cors());
 app.use(express.json());
 
 
+// A0cLegwkpA9X32CH
 // tarikulsk4
-// moV9Whif8cth9Icg
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://tarikulsk4:A0cLegwkpA9X32CH@cluster0.pisjypu.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const database = client.db("applyDB");
+        const applyCollection = database.collection("apply");
+
+        app.get('/apply', async (req, res) => {
+            const applied = applyCollection.find();
+            const result = await applied.toArray();
+            res.send(result)
+        })
+
+        app.post(('/apply'), async (req, res) => {
+            const apply = req.body;
+            console.log('Application Successful', apply);
+            const result = await applyCollection.insertOne(apply);
+            res.send(result)
+        })
 
 
 
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://tarikulsk4:moV9Whif8cth9Icg@cluster0.oqsrwob.mongodb.net/?retryWrites=true&w=majority";
 
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//     serverApi: {
-//         version: ServerApiVersion.v1,
-//         strict: true,
-//         deprecationErrors: true,
-//     }
-// });
+
 
 // async function run() {
 //     try {
@@ -59,6 +94,9 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Simple Crud is Running')
 
+})
+app.get('/colleges', (req, res) => {
+    res.send(colleges)
 })
 
 app.listen(port, () => {
